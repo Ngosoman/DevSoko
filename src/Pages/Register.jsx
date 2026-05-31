ec975e439dedf984ea17847a2dac105e4ec1fab0
Tom Mwambingu <[tomchom30@gmail.com]>
2026-05-31 16:04:22 +0300

    Updated Register page with minor text changes

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { supabase } from "../supabaseClient";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 import {
   Loader2,
   AlertCircle,
